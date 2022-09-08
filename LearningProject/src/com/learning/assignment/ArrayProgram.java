b9b04b2fede33f44ed8c13417975fdc84d575a7e
ashu143k <ashish.pathak143k@gmail.com>
2022-09-08 22:33:49 +0530

    ArrayProgram as new class

---
+++ LearningProject/src/com/learning/assignment/ArrayProgram.java
@@ -0,0 +1,14 @@
+package com.learning.assignment;
+
+public class ArrayProgram {
+
+	public static void main(String[] args) {
+		int[] a= {10,20,30,40,50,60};
+		for (int x:a)
+		{
+			System.out.println(x);
+		}
+
+	}
+
+}
